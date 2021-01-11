5f8d9d02ee897fa5adf2b4c3404c22a120885561
codervazgen <vzgogopro123@gmail.com>
2021-01-11 10:26:25 +0400

    commit 5

---
+++ product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def page_not_found(request, pnf):
+    return render(request, 'page_not_found.html', {})
+
+def HomePage(request):
+    return render(request, 'index.html', {})
